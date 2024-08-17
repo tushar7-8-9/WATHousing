485062e23d154c7c3ce1c23f4aa5c5867e823bd1
tushar7-8-9 <154453089+tushar7-8-9@users.noreply.github.com>
2024-08-17 19:32:18 +0000

    main files added

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-  return 'Hello from Flask!'
+  return render_template('home.html')
 
 
 if __name__ == '__main__':
